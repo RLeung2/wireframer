1fec292c926d59b6d960c68d1baf18dbcbae398c
Robert Leung <robert.j.leung@stonybrook.edu>
2020-11-15 20:09:54 -0500

    Updated Firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 const firebaseConfig = {
-    apiKey: "AIzaSyCRG0MRGtuyA0lhw95daVBs95JxAP8mU9s",
-    authDomain: "wireframe-7ba73.firebaseapp.com",
-    databaseURL: "https://wireframe-7ba73.firebaseio.com",
-    projectId: "wireframe-7ba73",
-    storageBucket: "wireframe-7ba73.appspot.com",
-    messagingSenderId: "716736296397",
-    appId: "1:716736296397:web:e326513e58290760170084",
-    measurementId: "G-M441Q9X9L3"
+    apiKey: "AIzaSyBZSyuFbjzk9edqyony9QUjVOfhe5cJj2g",
+    authDomain: "wireframe4-ecce8.firebaseapp.com",
+    databaseURL: "https://wireframe4-ecce8.firebaseio.com",
+    projectId: "wireframe4-ecce8",
+    storageBucket: "wireframe4-ecce8.appspot.com",
+    messagingSenderId: "413361951397",
+    appId: "1:413361951397:web:351330f1a86412c73d334c",
+    measurementId: "G-WKRC9HH4S3"
 };
 firebase.initializeApp(firebaseConfig);
 
