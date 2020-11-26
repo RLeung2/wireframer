3a42bd0ab5da9958e61e2bcacc54a6520a052e87
Robert Leung <robert.j.leung@stonybrook.edu>
2020-11-26 00:56:02 -0500

    Updates

---
+++ precache-manifest.733d7f66523adc5ec3e95d91b0fdfc82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20125c32aede8f030ee2400be8717343",
+    "url": "/wireframer/index.html"
+  },
+  {
+    "revision": "5479b4473faf9c26fecb",
+    "url": "/wireframer/static/css/main.f49b8f9d.chunk.css"
+  },
+  {
+    "revision": "8258dc4d9f0599ed9297",
+    "url": "/wireframer/static/js/2.517bdd8f.chunk.js"
+  },
+  {
+    "revision": "5479b4473faf9c26fecb",
+    "url": "/wireframer/static/js/main.b7cfcf47.chunk.js"
+  },
+  {
+    "revision": "a099befb410c91c34550",
+    "url": "/wireframer/static/js/runtime-main.f311fdde.js"
+  },
+  {
+    "revision": "4a0d5a293824b878235c3a21e2fff3ad",
+    "url": "/wireframer/static/media/UMBXrPdOoHOnxExyjdBeai3dAw.4a0d5a29.woff2"
+  },
+  {
+    "revision": "5a7bd2923196703ffe3a091f9c6a393a",
+    "url": "/wireframer/static/media/UMBXrPdOoHOnxExyjdBeaiLdA4M5.5a7bd292.woff2"
+  },
+  {
+    "revision": "e12c9c5a15a9e1ac8eaa94ef252c94a3",
+    "url": "/wireframer/static/media/UMBXrPdOoHOnxExyjdBeaiPdA4M5.e12c9c5a.woff2"
+  },
+  {
+    "revision": "967655a8e527554bd2def7c3d119232a",
+    "url": "/wireframer/static/media/border.967655a8.png"
+  },
+  {
+    "revision": "f774f8a4a0d806492e1ea79940ad59aa",
+    "url": "/wireframer/static/media/zoomin.f774f8a4.png"
+  },
+  {
+    "revision": "8ecc5e67b895276353c38c6d9d7af3fa",
+    "url": "/wireframer/static/media/zoomout.8ecc5e67.png"
+  }
+]);
